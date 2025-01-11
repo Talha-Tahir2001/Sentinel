e31a4e0681d7ca8e7250be376349943affbaa181
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-01-11 22:50:26 +0500

    Update App Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { FeaturesComponent } from "./components/features/features.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NavbarComponent, FooterComponent, FeaturesComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styles: [],
 })
 export class AppComponent {
   title = 'Sentinel';
+
+  isDarkMode = signal(false);
+
+  toggleTheme() {
+    this.isDarkMode.update(value => !value);
+  }
 }
