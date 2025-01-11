b8b76e0b0e4979d2c56937ac0862a1cf23c1baff
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-01-12 01:27:41 +0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'analyze',
+        loadComponent: () => import('./pages/analyze/analyze.component').then(m => m.AnalyzeComponent),
+    },
+    {
+        path: '**', 
+        redirectTo: ''
+    }
+];
