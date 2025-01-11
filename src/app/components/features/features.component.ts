b754f9e1b4865028bd738ef295c3eeb51a68140f
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-01-11 22:51:01 +0500

    Add Features Component

---
+++ src/app/components/features/features.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FeatureCardComponent } from "../feature-card/feature-card.component";
+
+@Component({
+  selector: 'app-features',
+  standalone: true,
+  imports: [FeatureCardComponent],
+  templateUrl: './features.component.html',
+  styles: ``
+})
+export class FeaturesComponent {
+
+}
