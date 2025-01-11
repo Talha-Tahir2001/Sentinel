ea541cbf3ca8a03911407e3b3f25f00d5f5e99af
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2025-01-12 02:58:01 +0500

    Update tailwind config to ES Module

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,41 +1,21 @@
 // /** @type {import('tailwindcss').Config} */
-// export const content = [
-//   "./src/**/*.{html,ts}",
-// ];
-// export const theme = {
-//   extend: {
-//     animation: {
-//       'float': 'float 6s ease-in-out infinite',
-//     }, keyframes: {
-//       float: {
-//         '0%, 100%': { transform: 'translateY(0)' },
-//         '50%': { transform: 'translateY(-20px)' },
-//       }
-//     }
-//   },
-// };
-// export const plugins = [];
-
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
-  theme: {
-    extend: {
-      animation: {
-        'float': 'float 6s ease-in-out infinite',
-      },
-      keyframes: {
-        float: {
-          '0%, 100%': { transform: 'translateY(0)' },
-          '50%': { transform: 'translateY(-20px)' },
-        }
-      }
+export const content = [
+  "./src/**/*.{html,ts}",
+];
+export const theme = {
+  extend: {
+    animation: {
+      'float': 'float 6s ease-in-out infinite',
     },
+    keyframes: {
+      float: {
+        '0%, 100%': { transform: 'translateY(0)' },
+        '50%': { transform: 'translateY(-20px)' },
+      }
+    }
   },
-  plugins: [require("daisyui")],
-  daisyui: {
-    themes: ["light", "dark"],
-  },
-}
+};
+export const plugins = [require("daisyui")];
+export const daisyui = {
+  themes: ["light", "dark"],
+};
